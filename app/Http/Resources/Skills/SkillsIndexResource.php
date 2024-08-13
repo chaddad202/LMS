ace823507d056c7700ca36624e990e10d17dabc2
chaddad202 <haddadchristina464@gmail.com>
2024-08-13 21:18:35 +0300

    skill_edit

--- app/Http/Resources/Skills/SkillsIndexResource.php
+++ app/Http/Resources/Skills/SkillsIndexResource.php
@@ -15,8 +15,11 @@ class SkillsIndexResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
-            'maximunPoint' => $this->maximunPoint,
+            'maximunBeginner' => $this->maximunBeginner,
+            'maximunIntemediate' => $this->maximunIntemediate,
+            'maximunAdvanced' => $this->maximunAdvanced,
 
         ];
     }
